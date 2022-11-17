b24d6ef78e2cc158b73627744102b7aa9d8facb1
waywarddaisy <jenna@waywarddaisy.com>
2022-11-17 08:03:23 -0800

    js efficiency updates

--- JS/projects.js
+++ JS/projects.js
@@ -10,85 +10,30 @@ let firstPara = document.getElementById("firstpara");
 let secondPara = document.getElementById("secondpara");
 let thirdPara = document.getElementById("thirdpara");
 
-const changeColor = ()=> {
+const changeColor = (domElement)=> {
 
-    firstTitle.style.color = "#f8b7cd"
+    domElement.style.color = "#f8b7cd"
 };
 
-const reset = ()=> {
+const reset = (domElement)=> {
 
-    firstTitle.style.color = "#0671b7"
+    domElement.style.color = "#0671b7"
 };
 
-firstTitle.onmouseover = changeColor;
-firstTitle.onmouseout = reset;
 
-const changeColorSecondTitle = ()=> {
+const twoEvents = (domElement) => {
 
-    secondTitle.style.color = "#f8b7cd"
+    domElement.addEventListener('mouseover', () => {changeColor(domElement);})
+    domElement.addEventListener('mouseout', () => {reset(domElement);})
 };
 
-const resetSecondTitle = ()=> {
+twoEvents(firstTitle);
+twoEvents(secondTitle);
+twoEvents(thirdTitle);
 
-    secondTitle.style.color = "#0671b7"
-};
-
-secondTitle.onmouseover = changeColorSecondTitle;
-secondTitle.onmouseout = resetSecondTitle;
-
-const changeColorThirdTitle = ()=> {
-
-    thirdTitle.style.color = "#f8b7cd"
-};
-
-const resetThirdTitle = ()=> {
-
-    thirdTitle.style.color = "#0671b7"
-};
-
-thirdTitle.onmouseover = changeColorThirdTitle;
-thirdTitle.onmouseout = resetThirdTitle;
-
-///
-
-const changeColorFirstPara = ()=> {
-
-    firstPara.style.color = "#f8b7cd"
-};
-
-const resetFirstPara = ()=> {
-
-    firstPara.style.color = "#0671b7"
-};
-
-firstPara.onmouseover = changeColorFirstPara;
-firstPara.onmouseout = resetFirstPara;
-
-const changeColorSecondPara = ()=> {
-
-    secondPara.style.color = "#f8b7cd"
-};
-
-const resetSecondPara = ()=> {
-
-    secondPara.style.color = "#0671b7"
-};
-
-secondPara.onmouseover = changeColorSecondPara;
-secondPara.onmouseout = resetSecondPara;
-
-const changeColorThirdPara = ()=> {
-
-    thirdPara.style.color = "#f8b7cd"
-};
-
-const resetThirdPara = ()=> {
-
-    thirdPara.style.color = "#0671b7"
-};
-
-thirdPara.onmouseover = changeColorThirdPara;
-thirdPara.onmouseout = resetThirdPara;
+twoEvents(firstPara);
+twoEvents(secondPara);
+twoEvents(thirdPara);
 
 
 function showAlert() {
