a58a335cd25541e100863d1d3a3e051fa324ae72
waywarddaisy <jenna@waywarddaisy.com>
2022-11-18 10:48:16 -0800

    js efficiency

--- JS/projects.js
+++ JS/projects.js
@@ -1,44 +1,22 @@
-let firstButton = document.getElementById("firstbutton");
-let secondButton = document.getElementById("secondbutton");
-let thirdButton = document.getElementById("thirdbutton");
-
-let firstTitle = document.getElementById("firsttitle");
-let secondTitle = document.getElementById("secondtitle");
-let thirdTitle = document.getElementById("thirdtitle");
-
-let firstPara = document.getElementById("firstpara");
-let secondPara = document.getElementById("secondpara");
-let thirdPara = document.getElementById("thirdpara");
-
-const changeColor = (domElement)=> {
-
-    domElement.style.color = "#f8b7cd"
-};
-
-const reset = (domElement)=> {
-
-    domElement.style.color = "#0671b7"
-};
-
+let allThreeButtons = document.querySelectorAll('button');
+let allThreeTitles = document.querySelectorAll('h1');
+let allThreeParagraphs = document.querySelectorAll('p');
 
 const twoEvents = (domElement) => {
 
-    domElement.addEventListener('mouseover', () => {changeColor(domElement);})
-    domElement.addEventListener('mouseout', () => {reset(domElement);})
+  domElement.addEventListener('mouseover', () => {domElement.style.color = "#f8b7cd"});
+  domElement.addEventListener('mouseout', () => {domElement.style.color = "#0671b7"});
 };
 
-twoEvents(firstTitle);
-twoEvents(secondTitle);
-twoEvents(thirdTitle);
-
-twoEvents(firstPara);
-twoEvents(secondPara);
-twoEvents(thirdPara);
-
+allThreeTitles.forEach(twoEvents);
+allThreeParagraphs.forEach(twoEvents);
 
 function showAlert() {
-    alert ("This project has not yet been completed");
-  }
-firstButton.onclick = showAlert;
-secondButton.onclick = showAlert;
-thirdButton.onclick = showAlert;
+  alert ("This project has not yet been completed");
+};
+
+const alertEvent=(domElement) => {
+  domElement.addEventListener('click', showAlert)
+};
+
+allThreeButtons.forEach(alertEvent);
